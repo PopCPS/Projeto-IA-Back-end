aed2ba701a4b447bc24ecb43ac0998f59d572b44
Artur Coelho <arturcoelho.cps@gmail.com>
2024-11-22 16:24:41 -0300

    finalização da rota de análise de aluno

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
 from flask import Flask, request, jsonify
-import xgboost as xgb
 import numpy as np
+import xgboost as xgb
+import pandas as pd
+import joblib
 
 app = Flask(__name__)
 
-model = xgb.XGBClassifier()
-model.load_model("xgboost_model.json")
+model = joblib.load('xgboost_model.pkl')
+
+one_hot_encoder = joblib.load('one_hot_encoder.pkl')
+
+categorical_features = ['day_of_class', 'time_of_class', 'category']
+numerical_features = ['months_as_member', 'weight', 'days_before_class']
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         input_data = request.json
+        if not input_data or not all(key in input_data for key in (categorical_features + numerical_features)):
+            return jsonify({"error": f"Invalid input. Please provide all the following features: {categorical_features + numerical_features}"}), 400
         
-        if not input_data or 'features' not in input_data:
-            return jsonify({"error": "Invalid input. Please provide 'features' as a list of numbers."}), 400
-        
-        features = np.array(input_data['features']).reshape(1, -1) 
-        
-        if features.shape[1] != model.n_features_in_:
-            return jsonify({"error": f"Expected {model.n_features_in_} features, but got {features.shape[1]}."}), 400
-        
-        prediction = model.predict(features)[0]
-        prediction_probabilities = model.predict_proba(features).tolist()
-        
+        data = pd.DataFrame([input_data])
+
+        numeric_data = data[numerical_features].values
+        categorical_data = one_hot_encoder.transform(data[categorical_features])
+
+        processed_features = np.hstack((numeric_data, categorical_data))
+
+        prediction = model.predict(processed_features)[0]
+        probabilities = model.predict_proba(processed_features)
+
         return jsonify({
-            "prediction": int(prediction),
-            "probabilities": prediction_probabilities
+            "prediction": int(prediction > 0.5),
+            "probabilities": probabilities.tolist()
         })
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
